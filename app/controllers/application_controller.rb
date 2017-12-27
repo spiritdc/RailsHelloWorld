c8a875625137be055127d33e7c65c6a6b2345c6a
Cheng Chen <cfc14_neer@hotmail.com>
2017-12-27 18:42:42 -0500

    Router practice

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
+# Application controller
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  # hello action
+  def hello
+    render html: '¡ Hola, mundo!'
+  end
+
+  # goodby action
+  def goodby
+    render html: 'Goodby, world!'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # router root to application/hello
+  # root 'application#hello'
+
+  # router root to application/goodby
+  root 'application#goodby'
 end
